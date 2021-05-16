dea5371aa1be4cdfedacb9d965015ba024aefa85
Shestak Sergey <shestak.sergey.a@gmail.com>
2021-05-16 13:42:37 +0300

    implement user endpoint

--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -4,8 +4,40 @@ const usersService = require('./user.service');
 
 router.route('/').get(async (req, res) => {
   const users = await usersService.getAll();
-  // map user fields to exclude secret fields like "password"
   res.json(users.map(User.toResponse));
+});
+
+router.route('/:id').get(async (req, res) => {
+  const {
+    params: { id },
+  } = req;
+  const user = await usersService.getUserById(id);
+  res.json(User.toResponse(user));
+});
+
+router.route('/').post(async (req, res) => {
+  const { body } = req;
+  const user = await usersService.createUser(body);
+  res.status(201);
+  res.json(User.toResponse(user));
+});
+
+router.route('/:id').put(async (req, res) => {
+  const {
+    params: { id },
+    body,
+  } = req;
+  const user = await usersService.updateUser(id, body);
+  res.json(User.toResponse(user));
+});
+
+router.route('/:id').delete(async (req, res) => {
+  const {
+    params: { id },
+  } = req;
+  await usersService.deleteUser(id);
+  res.status(204);
+  res.json();
 });
 
 module.exports = router;
--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,5 +1,9 @@
 const usersRepo = require('./user.memory.repository');
 
 const getAll = () => usersRepo.getAll();
+const getUserById = (id) => usersRepo.getUserById(id);
+const createUser = (userData) => usersRepo.createUser(userData);
+const updateUser = (id, userData) => usersRepo.updateUser(id, userData);
+const deleteUser = (id) => usersRepo.deleteUser(id);
 
-module.exports = { getAll };
+module.exports = { getAll, getUserById, createUser, updateUser, deleteUser };
